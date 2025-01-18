954746c9a2bba03e9a653739f9a32eb25f808829
samsamisami <uktiktok43@gmail.com>
2025-01-18 14:49:27 +0500

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import HeroSection from "@/components/ui/hero-section"
-import TestimonialsSection from "@/components/ui/testimonial-section"
-import ResultsSection from "@/components/ui/result-section"
 import ImpactSection from "@/components/stats"
 import ProcessSection from "@/components/ui/process-section"
 
@@ -9,8 +7,6 @@ export default function Home() {
     <main>
       <HeroSection />
       <ProcessSection/>
-      <ResultsSection/>
-      <TestimonialsSection/>
       <ImpactSection/>
 
     </main>
